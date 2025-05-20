4b3c62830d812ed31c81f319d363300f77cacb32
elgatito9612 <kajawaw056@gmail.com>
2025-05-20 16:29:03 +0200

    feat: gui with interactive map, crud fully working

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 
 import tkintermapview
-from tkintermapview import TkinterMapView
 
 users:list = []
 
@@ -12,6 +11,7 @@ class User:
         self.location=location
         self.posts=posts
         self.coordinates= self.get_coordinates()
+        self.marker=map_widget.set_marker(self.coordinates[0], self.coordinates[1])
 
 
     def get_coordinates(self) -> list:
@@ -34,7 +34,7 @@ def add_user():
     miejscowosc = entry_location.get()
     tmp_user=User(name=imie, surname=nazwisko, location=miejscowosc, posts=posty)
     users.append(tmp_user)
-    map_widget.set_marker(tmp_user.coordinates[0], tmp_user.coordinates[1], text=tmp_user.location)
+
     print(users)
     entry_name.delete(0, END)
     entry_surname.delete(0, END)
@@ -51,6 +51,7 @@ def show_users():
 
 def delete_user():
     idx=listbox_lista_obiektow.index(ACTIVE)
+    users[idx].marker.delete()
     users.pop(idx)
     show_users()
 
@@ -60,6 +61,8 @@ def user_details():
     label_surname_szczegoly_obiektow_wartosc.configure(text=users[idx].surname)
     label_posts_szczegoly_obiektow_wartosc.configure(text=users[idx].posts)
     label_location_szczegoly_obiektow_wartosc.configure(text=users[idx].location)
+    map_widget.set_position(users[idx].coordinates[0],users[idx].coordinates[1])
+    map_widget.set_zoom(16)
 
 def edit_user():
     idx=listbox_lista_obiektow.index(ACTIVE)
@@ -76,10 +79,15 @@ def update_user(idx):
     location=entry_location.get()
     posts=entry_posts.get()
 
+
     users[idx].name=name
     users[idx].surname=surname
     users[idx].location=location
     users[idx].posts=posts
+
+    users[idx].marker.delete()
+    users[idx].coordinates=users[idx].get_coordinates()
+    users[idx].marker=map_widget.set_marker(users[idx].coordinates[0],users[idx].coordinates[1])
 
     button_dodaj_obiekt.configure(text="Dodaj", command=add_user)
     show_users()
